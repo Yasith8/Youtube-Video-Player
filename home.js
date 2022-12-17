246da364b566778e46db07956f90d0bf0c8f261d
Yasith Nawodya <yasithnawodya8@gmail.com>
2022-12-17 19:48:12 +0530

    Create home.js

---
+++ home.js
@@ -0,0 +1,23 @@
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '360',
+        width: '640',
+        videoId: 'dVYl5ImNjow', // The YouTube video ID
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.ENDED) {}
+}
